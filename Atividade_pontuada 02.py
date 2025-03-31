b0ecb604365db3f9b8c96811522f872bd2008ae7
eulskkj <pikapaybeludo@gmail.com>
2025-03-31 23:27:10 +0000

    finalizado

--- Atividade_pontuada 02.py
+++ Atividade_pontuada 02.py
@@ -6,6 +6,11 @@ os.system ("clear || cls")
 #atividade pontuada
 
 total = 0
+junta_prato = ""
+pratos_escolhidos = []
+
+
+
 
 while True:
     #mostrando a tabela
@@ -24,25 +29,35 @@ Digite o código do seu pedido:
     #pedindo o prato       
     match codigo:
         case 1:
+            nome = "Picanha"
             valor = 25
         case 2:
+            nome = "Lasanha"
             valor = 20
         case 3:
+            nome = "Strogonoff"
             valor = 18
         case 4:
+            nome = "Bife acebolado"
             valor = 15
         case 5:
+            nome = "Pão com ovo"
             valor = 5
         case 6:
+            nome = "Moqueca de peixe"
             valor = 21
         case 7:
+            nome = "Feijoada"
             valor = 22
         case _:
             print("Opção inválida")
+            continue
+         
      #Armazendando os valores   
     total += valor
+    pratos_escolhidos.append(f"{codigo} - {nome}")
     #Pedindo outros pratos 
-    outro_pedido = input("Deseja pedir algo mais? (digite 0 caso não queira e 1 para continuar): ")  
+    outro_pedido = input("Deseja pedir algo mais? (digite 0 caso não queira e 1 para continuar.): ")  
 
     #Terminando o pedido
     if outro_pedido == "0":
@@ -56,23 +71,28 @@ CÓDIGO |      FORMA
    1   |      Á vista 
    2   |  Cartão de crédito
     
-    Informe o código da forma de pagamento: """))
-match forma_pagamento:
-        #calculando desconto
-    case 1:
-            desconto = total * 0.10
-            valor_pagar = total - desconto
-        #calculando o acréscimo
-    case 2:
-            desconto = total * 0.10 
-            valor_pagar = total + desconto
-junta_prato = ""
-junta_prato += codigo + " "
+Informe o código da forma de pagamento:  """))
 
+#calculando desconto
+if forma_pagamento == 1:
+    desconto = total * 0.10
+    valor_pagar = total - desconto
+#calculando o acréscimo
+elif forma_pagamento == 2:
+    desconto = total * 0.10 
+    valor_pagar = total + desconto
+else: 
+    print("Escolha 1 para pagamento á vista ou 2 para pagamento de cartão de credito.")
+    
 
 #exibindo resultados
-print (f"Pratos: {junta_prato}")
-print (f"Subtotal: {total}")
+print("\n=== RESUMO DO PEDIDO ===")
+print("Pratos escolhidos:")
+for prato in pratos_escolhidos:
+    print(prato)
+print (f"Pratos escolhidos: {junta_prato}")
 print (f"Forma de pagamento escolhida: {forma_pagamento}")
-print (f"Total do desconto ou acréscimo: {desconto}")
-print (f"Valor final: {valor_pagar}")
+print (f"Subtotal: R${total:.2f}")
+print (f"Total do desconto ou acréscimo: R${desconto:.2f}")
+print (f"Valor final: R${valor_pagar:.2f}")
+print(f"Obrigado pela preferência e um bom apetite.")
